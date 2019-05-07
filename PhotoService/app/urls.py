eb29d416e367cee3f80d12d602ad8eba2f4bd125
yuki0508 <enla.festa.la.vita@icloud.com>
2019-05-07 14:26:41 +0900

    Userモデル

--- PhotoService/app/urls.py
+++ PhotoService/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('users/<int:pk>/', views.users_detail, name='users_detail'),  #←追加
 ]
--- PhotoService/app/views.py
+++ PhotoService/app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     #requestが第一引数,第二引数はtemplatesフォルダからみてどこのファイルを参照するか
     return render(request, 'app/index.html')
+
+def users_detail(request,pk):
+    user = get_object_or_404(User, pk=pk)
+    return render(request, 'app/users_detail.html', {'user': user})
