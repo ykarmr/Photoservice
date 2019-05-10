64a05438897e1b680db573fb06a41d3a18995d5b
yuki0508 <enla.festa.la.vita@icloud.com>
2019-05-10 13:49:54 +0900

    signup機能の追加

--- PhotoService/app/views.py
+++ PhotoService/app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.models import User
 from .models import Photo
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def index(request):
     #requestが第一引数,第二引数はtemplatesフォルダからみてどこのファイルを参照するか
@@ -13,3 +14,21 @@ def users_detail(request,pk):
     user = get_object_or_404(User, pk=pk)
     photos = user.photo_set.all().order_by('-created_at')
     return render(request, 'app/users_detail.html', {'user': user,'photos':photos})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST) # ユーザーインスタンスを作成
+        if form.is_valid():
+            new_user = form.save() # ユーザーインスタンスを保存
+            input_username = form.cleaned_data['username']
+            input_password = form.cleaned_data['password1']
+            # フォームの入力値で認証できればユーザーオブジェクト、できなければNoneを返す
+            new_user = authenticate(username=input_username, password=input_password)
+            # 認証成功時のみ、ユーザーをログインさせる
+            if new_user is not None:
+                # loginメソッドは、認証ができてなくてもログインさせることができる。→上のauthenticateで認証を実行する
+                login(request, new_user)
+                return redirect('app:users_detail', pk=new_user.pk)
+    else:
+        form = UserCreationForm()
+    return render(request, 'app/signup.html', {'form': form})
